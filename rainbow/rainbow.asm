79b6e86a018d56e49c6630ba2cf81aa18cf285f3
Johan Fredin <johan.fredin.2@gu.se>
2024-12-30 21:24:03 +0100

    rainbow example

---
+++ rainbow/rainbow.asm
@@ -0,0 +1,72 @@
+    PROCESSOR 6502
+
+    include "../common/vcs.h"
+    include "../common/macro.h"
+
+    seg code
+    org $F000       ; defines the origin of the ROM at $F000
+
+START:
+    CLEAN_START     ; Macro to safely clear the memory
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start a new frame by turning on VBLANK and VSYNC
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+NextFrame:
+    lda #2          ; same as binary value %00000010
+    sta VBLANK      ; turn on VBLANK
+    sta VSYNC       ; turn on VSYNC
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Generate the 3 lines of VSYNC
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    sta WSYNC       ; first scanline
+    sta WSYNC       ; second scanline
+    sta WSYNC       ; third scanline
+
+    lda #0
+    sta VSYNC       ; turn off VSYNC
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Let the TIA output the recommended 37 scanlines of VBLANK
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ldx #37         ; X = 37 (to count 37 scanlines)
+LoopVBlank:
+    sta WSYNC       ; hit WSYNC and wait for the next scanline
+    dex             ; X--
+    bne LoopVBlank  ; loop while X != 0
+
+    lda #0
+    sta VBLANK      ; turn off VBLANK
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Draw 192 visible scanlines (kernel)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ldx #192            ; Counter for 192 visible scanlines
+LoopVisible:
+    stx COLUBK          ; set the bg color
+    sta WSYNC           ; wait for the next scanline
+    dex                 ; x--
+    bne LoopVisible     ; Loop while x != 0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Output 30 more VBLANK lines (overscan to complete our frame)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #2              ; hit and turn on VBLANK again
+    sta VBLANK
+
+    ldx #30             ; counter for 30 scanlines
+LoopOverScan:
+    sta WSYNC           ; wait for the next scanline
+    dex                 ; x--
+    bne LoopOverScan    ; loop while X != 0
+
+    jmp NextFrame
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Complete my ROM size to 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    .org $FFFC      
+    .word START
+    .word START
